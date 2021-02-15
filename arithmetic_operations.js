35655d00c13abf7141da80941227920fbdb5e368
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-15 21:29:06 +0530

    Program to perform Arithmetic Operations

---
+++ arithmetic_operations.js
@@ -0,0 +1,7 @@
+//A JavaScript program to perform Arithmetic Operations
+console.log('Sum is:',3.5 + 25);
+console.log('Subtraction is:',2021 - 1969);
+console.log('Division is:',65 / 240);
+console.log('Multiplication is:',0.2708 * 100);
+console.log('Exponential is:', 3 ** 4);
+console.log('Exponential is:', (2 ** 3) ** 2);
