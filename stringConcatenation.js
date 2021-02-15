354af88d4b7cb5a767b9798cca4063a82e9574a7
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-15 21:24:03 +0530

    Program to perform string concatenation

---
+++ stringConcatenation.js
@@ -0,0 +1,6 @@
+//A JavaScript Program to perform string concatenation
+console.log('Hello' + 'World');                                   //Output-:HelloWorld
+console.log('Hello ' + 'World');                                 //Output-:Hello World
+console.log('Hello' + ' World');                                //Output-:Hello World
+console.log('Hello' + ' ' + 'World');                          //Output-:Hello World
+console.log('One' + ', ' + 'two' + ', ' + 'three!');          //Output-:One, two, three!
