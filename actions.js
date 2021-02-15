d68b25e0762af4b040bb7a82ec84c51833f8424b
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-15 21:30:31 +0530

    Program to perform actions on string

---
+++ actions.js
@@ -0,0 +1,5 @@
+//A JavaScript program to perform actions on string
+console.log('pratap'.toUpperCase());                        //Output-:PRATAP
+console.log('YOGENDRA'.toLowerCase());                     //Output-:yogendra
+console.log('Yogendra'.startsWith('Y'));                  //Output-:true
+console.log('    Remove whitespace   '.trim());          //Output-:Remove whitespace
