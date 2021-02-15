86db7ab7051fb4f7dc3d1727d062964bae83f7ac
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-15 21:28:03 +0530

    Program to demonstrate Math.random()

---
+++ mathRandom.js
@@ -0,0 +1,12 @@
+//A JavaScript program to demonstrate Math.random()
+console.log(Math.random()); 
+// Here Math.random() will generata any random number between 0 & 1
+
+console.log(Math.random() * 50);
+//Here Math.random() will generata any random number between 0 & 1 and will multiply that number by 50. So now we have a number between 0 & 50
+
+console.log(Math.floor(Math.random() * 50));
+//Here Math.floor rounds the number down to the nearest whole number
+
+console.log(Math.E);                    //Euler's Number
+console.log((1 + (1 / 1000000)) ** 1000000);
