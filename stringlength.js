e25abe30bf577a1f90ae8d501813f815b7303766
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-15 21:23:23 +0530

    Program to find the length of the given string

---
+++ stringlength.js
@@ -0,0 +1,3 @@
+//A JavaScript program to find the length of the given string
+console.log('PRATAPSINGH'.length);                    //Output-:11
+console.log('Yogendra Pratap Singh'.length);         //Output-:21
