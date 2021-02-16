3f6cd449dc30505e277c3202ada48b50138a6716
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-16 13:08:08 +0530

    Program for String Concatenation using Variables

---
+++ stringConcatenationUsingVariable.js
@@ -0,0 +1,3 @@
+//A JavaScript program for String Concatenation using Variables
+let myLastName = 'Pratap Singh';
+console.log('Yogendra ' + myLastName);                      //Output-:Yogendra Pratap Singh
