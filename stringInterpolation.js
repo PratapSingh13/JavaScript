c1fd845ad82153820f3934290e2d48d83f4a7b06
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-16 13:32:29 +0530

    Program to demonstrate String Interpolation

---
+++ stringInterpolation.js
@@ -0,0 +1,10 @@
+//A JavaScript program to demonstrate String Interpolation
+const myLastName = 'Pratap Singh';
+console.log(`My name is: ${myLastName}`);                    //Output-:My name is: Pratap Singh
+
+let myFirstName = 'Yogendra';
+console.log(`My name is: ${myFirstName} Pratap Singh`);     //Output-:My name is:Yogendra Pratap Singh
+
+const myName = 'Yogendra Pratap Singh';
+const myCity = 'Frankfurt';
+console.log(`My name is: ${myName}. My favorite city is: ${myCity}`)                 //Output-:My name is Yogendra Pratap Singh. My favorite city is Frankfurt
