99be1b7acca38efe18c3ad48c0b4e43a71705b98
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-16 12:11:28 +0530

    Program to demonstrate the features of 'let' variable

---
+++ let.js
@@ -0,0 +1,16 @@
+//A JavaScript program to demonstrate the features of 'let' variable
+//The main feature of 'let' variable is we can re-assign that same variable with another value
+let price;                                    //Variable 'price' initialized with value 'undefined' automatically
+console.log(price);                          //Output-:undefined
+price = 350;                                //Here we can re-assign the value in variable 'price'
+console.log(price);                        //Output-:350
+
+let meal = 'Pizza';
+console.log(meal);                       //Output-:Pizza
+meal = 'Doritos';                       //Here we can re-assign the value in variable 'meal'
+console.log(meal);                     //Output-:Doritos
+
+let changeMe = true;
+console.log(changeMe);               //Output-:true
+changeMe = false;                   //Here we can re-assign the value in variable 'changeMe'
+console.log(changeMe);             //Output-:false
