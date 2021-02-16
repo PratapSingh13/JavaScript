15b298e279501a3c12c00ea9828078f24d6e200c
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-16 23:11:26 +0530

    Program to demonstrate else...if conditions

---
+++ trafficRules.js
@@ -0,0 +1,15 @@
+//A JavaScript program to demonstrate else...if conditions
+let stopLight = 'Yellow';
+
+if(stopLight === 'Red')
+{
+    console.log('Stop!');
+}
+else if(stopLight === 'Yellow')
+{
+    console.log('Slow down!');
+}
+else
+{
+    console.log('Go!');
+}
