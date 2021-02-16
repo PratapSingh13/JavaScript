554a4d346c1282eeef3464ce79fc2f2ffe3ad5b0
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-16 13:48:00 +0530

    Program to find the type of 'DataType'

---
+++ typeOf.js
@@ -0,0 +1,9 @@
+//A JavaScript program to find the type of 'DataType'
+const myName = 'Yogendra Pratap Singh';
+console.log(typeof myName);
+
+const myAge = 25;
+console.log(typeof myAge);
+
+let myAddress = 'Kanpur';
+console.log(typeof myAddress)
