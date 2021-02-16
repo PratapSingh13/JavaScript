2a9023c77bb61958ae40706e8ecb1d26067cf46f
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-16 12:10:56 +0530

    Program to demonstrate the features of 'const' variable

---
+++ const.js
@@ -0,0 +1,4 @@
+//A JavaScript program to demonstrate the features of 'const' variable
+//The main feature of 'const' variable is we can not re-assign that same variable with another value
+const myName = 'Yogendra Pratap Singh';
+console.log(myName);
