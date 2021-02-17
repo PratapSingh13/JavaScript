7f3fb5a96dcb2f4882cdb43499830a84bf54b0eb
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-17 15:28:48 +0530

    Program for introduction of Functions

---
+++ introFunctions.js
@@ -0,0 +1,6 @@
+//A JavaScript program for introduction of Functions
+function sayThanks(name) 
+{
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+sayThanks('Yogendra Pratap Singh');
