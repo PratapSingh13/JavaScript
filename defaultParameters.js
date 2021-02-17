e1dccc5e711bfb3dfd174b9af66578c4e9f0fe81
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-17 11:34:58 +0530

    Program to show the use case of 'default parameters' in functions

--- defaultParameters.js
+++ defaultParameters.js
@@ -14,13 +14,25 @@ Remember to buy bread
 Remember to buy eggs
 */
 
+function makeShoppingList(item1='milk', item2='bread', item3='eggs')
+{
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+makeShoppingList(item1='cake', item2='butter', item3='coke');
 
+/*
+Output-:
+Remember to buy cake
+Remember to buy butter
+Remember to buy coke
+*/
 
 function greeting (name = 'stranger') 
 {
   console.log(`Hello, ${name}!`)
 }
- 
 greeting('Yogendra')                     // Output: Hello, Yogendra!
 greeting()                              // Output: Hello, stranger!
  
