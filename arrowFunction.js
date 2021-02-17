418d5a81e5863fbdd8b3323037cb5000a094bff2
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-17 22:26:26 +0530

    Program to demonstrate 'ArrowFunction' in JavaScript

---
+++ arrowFunction.js
@@ -0,0 +1,16 @@
+/*
+Arrow functions remove the need to type out the keyword function every time you need to create a function. 
+Instead, you first include the parameters inside the ( ) and then add an arrow => that points to the function body surrounded in { }
+*/
+const plantNeedsWater = (day) => 
+{
+  if (day === 'Wednesday') 
+  {
+    return true;
+  } 
+  else 
+  {
+    return false;
+  }
+}
+console.log(plantNeedsWater('Tuesday'));
