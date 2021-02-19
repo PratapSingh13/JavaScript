3d4a11b1602e3d02f24ae41135a24cc5f8c4c01f
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-19 23:11:40 +0530

    Program to understand .push()

---
+++ pushMethod.js
@@ -0,0 +1,5 @@
+//A JavaScript program to understand .push()
+//.push() allows us to add items to the end of an array.
+const itemTracker = ['item 0', 'item 1', 'item 2'];
+itemTracker.push('item 3', 'item 4');
+console.log(itemTracker);                         //Output-:['item 0', 'item 1', 'item 2', 'item 3', 'item 4']
