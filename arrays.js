db3c306420f8740d33a7331c4d030e6ccca4fff3
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-19 23:12:29 +0530

    push method added

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
+//A JavaScript program to understand .length property in array
 //.length property in Arrays
 const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
 console.log(objectives.length);              //Output-:3
+
+//A JavaScript program to understand .push()
+//.push() allows us to add items to the end of an array.
+const itemTracker = ['item 0', 'item 1', 'item 2'];
+itemTracker.push('item 3', 'item 4');
+console.log(itemTracker);                         //Output-:['item 0', 'item 1', 'item 2', 'item 3', 'item 4']
