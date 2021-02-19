fff8222dd4a581f644af9dfa458b3ff1d68c908c
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-19 23:17:02 +0530

    Program to understand .pop()

---
+++ popMethod.js
@@ -0,0 +1,6 @@
+//A JavaScript program to understand .pop()
+//In JavaScript .pop() removes the last item of an arra
+const newItemTracker = ['item 0', 'item 1', 'item 2'];
+const removed = newItemTracker.pop();
+console.log(newItemTracker);              //Output-: [ 'item 0', 'item 1' ]
+console.log(removed);                    //Output-: item 2
