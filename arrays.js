f9c7a58c52bf71a36d61d1eb7ee410fa154d6594
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-02-19 23:17:48 +0530

    pop method created for arrays

--- arrays.js
+++ arrays.js
@@ -8,3 +8,10 @@ console.log(objectives.length);              //Output-:3
 const itemTracker = ['item 0', 'item 1', 'item 2'];
 itemTracker.push('item 3', 'item 4');
 console.log(itemTracker);                         //Output-:['item 0', 'item 1', 'item 2', 'item 3', 'item 4']
+
+//A JavaScript program to understand .pop()
+//In JavaScript .pop() removes the last item of an arra
+const newItemTracker = ['item 0', 'item 1', 'item 2'];
+const removed = newItemTracker.pop();
+console.log(newItemTracker);              //Output-: [ 'item 0', 'item 1' ]
+console.log(removed);                    //Output-: item 2
